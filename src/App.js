3ec67d0c1fc76b286d3a81477ab01010d2a784f3
Ana Nikolova <ani.nik19@gmail.com>
2020-12-06 15:45:19 +0100

    working on searchBar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import { Grid } from "@material-ui/core";
 
 import SearchBar from "./components/SearchBar";
 import VideoDetails from "./components/VideDetails";
+import youtube from "./api/youtube";
 import "./App.css";
 
 class App extends React.Component {
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: ["API_KEY"],
+        q: searchTerm,
+      },
+    });
+    console.log(response.data.items);
+  };
+
   render() {
     return (
       <Grid justify="center" container spacing={15}>
         <Grid item xs={12}>
-          <Grid constainer spacing={16}>
+          <Grid constainer spacing={10}>
             <Grid item xs={12}>
-              <SearchBar />
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={8}>
               <VideoDetails />
